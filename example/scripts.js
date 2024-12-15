6df5f221ac34b00b655b4251ce971cfc0a492ad6
ar.heidari <ar.heidari@dotin.ir>
2024-12-15 08:53:48 +0330

    -

--- example/scripts.js
+++ example/scripts.js
@@ -1,61 +1,67 @@
 let bgUrl = null
-document.getElementById('brightnessRange').addEventListener('change', function () {
-    const value = this.value;
-    bgModifier.setBackgroundImage(bgUrl).setBrightness(value).setStream(stream).changeBackground(outputVideo);
-});
-
-document.getElementById('contrastRange').addEventListener('change', function () {
-    const value = this.value;
-    bgModifier.setBackgroundImage(bgUrl).setContrast(value).setStream(stream).changeBackground(outputVideo);
-});
-
-document.getElementById('grayScaleSwitch').addEventListener('change', function () {
-    const value = this.checked;
-    bgModifier.setBackgroundImage(bgUrl).setGrayScale(value).setStream(stream).changeBackground(outputVideo);
-});
-// Get the webcam video stream
-const stream = await navigator.mediaDevices.getUserMedia({video: true});
+let newStream = null
 const outputVideo = document.getElementById("output");
+const stream = await navigator.mediaDevices.getUserMedia({video: true});
 outputVideo.srcObject = stream
 outputVideo.play();
+
+document.getElementById('brightnessRange').addEventListener('change', async function () {
+    const value = this.value;
+    outputVideo.srcObject = await bgModifier.setBackgroundImage(bgUrl).setBrightness(value).setStream(stream).changeBackground();
+});
+
+document.getElementById('contrastRange').addEventListener('change', async function () {
+    const value = this.value;
+    outputVideo.srcObject = await bgModifier.setBackgroundImage(bgUrl).setContrast(value).setStream(stream).changeBackground();
+});
+
+document.getElementById('blurRange').addEventListener('change', async function () {
+    const value = this.value;
+    outputVideo.srcObject = await bgModifier.setBackgroundImage(bgUrl).setBlur(value).setStream(stream).changeBackground();
+});
+
+document.getElementById('grayScaleSwitch').addEventListener('change', async function () {
+    const value = this.checked;
+    outputVideo.srcObject = await bgModifier.setBackgroundImage(bgUrl).setGrayScale(value).setStream(stream).changeBackground();
+});
+// Get the webcam video stream
 
 
 import WebrtcBgModifier from "../dist/index.module.js";
 
 const bgModifier = new WebrtcBgModifier();
-window.changeBg = function (url) {
+window.changeBg = async function (url) {
     bgUrl = url;
-    bgModifier.setBackgroundImage(url).setStream(stream).changeBackground(outputVideo);
+    newStream = await bgModifier.setBackgroundImage(url).setStream(stream).changeBackground();
+    outputVideo.srcObject = newStream;
+
 };
-window.changeBgColor = function (color) {
-    bgModifier.setBackgroundColor(color).setStream(stream).changeBackground(outputVideo);
+window.changeBgColor = async function (color) {
+    newStream = await bgModifier.setBackgroundColor(color).setStream(stream).changeBackground();
+    outputVideo.srcObject = newStream;
+
 };
 
 
-let bgListHtml = bgModifier.backgroundList()
-    .map(item =>
-        [
-            ...item.color.map(d =>
-                `<div onclick="changeBgColor('${d.value}')">
-                    <div style="width: 100px; height: 80px; background: ${d.value}; display: flex; align-items: center; justify-content: center; color: #333; border: 1px solid #ddd; border-radius: 8px;">
-                        ${d.alt}
-                    </div>
-                </div>`
-            ),
-            ...item.image.map(d =>
-                `<div onclick="changeBg('${d.value}')">
-                    <img width="100" src="${d.value}" alt="${d.alt}" style="border: 1px solid #ddd; border-radius: 8px;">
-                </div>`
-            )
-        ].join('')
-    )
+const bgListItems = bgModifier.backgroundList()
+let bgListHtml = `<div onclick="changeBg(null)"> <video width="100px"   autoplay playsinline id="default-video" /> </div>`;
+
+bgListHtml += bgListItems.color.map(d =>
+    `<div onclick="changeBgColor('${d.value}')">
+                        <div style="width: 100px; height: 80px; background: ${d.value}; display: flex; align-items: center; justify-content: center; color: #333; border: 1px solid #ddd; border-radius: 8px;">
+                            ${d.alt}
+                        </div>
+                    </div>`
+).join('')
+bgListHtml += bgListItems.image.map(d =>
+    `<div onclick="changeBg('${d.value}')">
+                        <img width="100" src="${d.value}" alt="${d.alt}" style="border: 1px solid #ddd; border-radius: 8px;">
+                    </div>`
+)
     .join('');
 
-bgListHtml += `<div onclick="changeBg(null)"> <video width="100px"   autoplay playsinline id="default-video" /> </div>`;
 document.getElementById("bg-list").innerHTML = bgListHtml
-bgModifier.setStream(stream).changeBackground(outputVideo);
 
 const defaultVideo = document.getElementById("default-video");
-
 defaultVideo.srcObject = stream
 defaultVideo.play();
--- src/index.js
+++ src/index.js
@@ -7,19 +7,23 @@ class WebrtcBgModifier {
         this.brightness = 1;
         this.contrast = 1;
         this.grayScale = false;
+        this.blur = 0;
         this.videoElement = document.createElement("video");
         this.canvasElement = document.createElement("canvas");
-
     }
 
+    // Setters for background properties
     setBrightness(value) {
         this.brightness = +value;
-
-         return this;
+        return this;
     }
 
     setContrast(value) {
         this.contrast = +value;
+        return this;
+    }
+    setBlur(value) {
+        this.blur = +value+"px";
         return this;
     }
 
@@ -44,51 +48,51 @@ class WebrtcBgModifier {
         return this;
     }
 
-    // Appends a script to the document head, ensuring it is loaded only once
+    // Appends a script to the document head, ensuring it loads only once
     async appendScriptToHead(src, {
         async = true, defer = false, callback = () => {
         }
     } = {}) {
         if (this.scriptLoaded) {
-            callback();
+            await  callback();
             return;
         }
-        ; // Skip if the script is already loaded
 
         const script = document.createElement('script');
         script.src = src;
         script.async = async;
         script.defer = defer;
-        script.onload = () => {
+        script.onload =async () => {
             this.scriptLoaded = true;
-            callback();
+          await  callback();
         };
         script.onerror = () => console.error(`Failed to load script: ${src}`);
         document.head.appendChild(script);
     }
 
-    changeVideoBackgroundImage(ctx, results, backgroundImage) {
+    // Handles background image replacement logic
+    applyBackgroundImage(ctx, results, backgroundImage) {
         const {videoWidth: width, videoHeight: height} = this.videoElement;
         ctx.clearRect(0, 0, width, height);
-        if (backgroundImage) {
-            ctx.drawImage(results.segmentationMask, 0, 0, width, height);
-            ctx.globalCompositeOperation = 'source-out';
-            ctx.drawImage(backgroundImage, 0, 0, width, height);
-            ctx.globalCompositeOperation = 'destination-atop';
+        ctx.filter = `brightness(${this.brightness}) contrast(${this.contrast}) blur(${this.blur})`;
 
-        } else {
-            ctx.drawImage(this.videoElement, 0, 0, width, height);
+        ctx.drawImage(results.segmentationMask, 0, 0, width, height);
+        ctx.globalCompositeOperation = 'source-out';
 
-        }
+        ctx.drawImage(backgroundImage ? backgroundImage : this.videoElement, 0, 0, width, height);
+        ctx.globalCompositeOperation = 'destination-atop';
+
+        ctx.filter = `brightness(${this.brightness}) contrast(${this.contrast})`
         ctx.drawImage(results.image, 0, 0, width, height);
     }
 
-    changeVideoBrightnessAndContrast(ctx) {
-        ctx.filter = `brightness(${this.brightness}) contrast(${this.contrast})`;
+    // Adjusts brightness and contrast for the video
+    applyBrightnessAndContrast(ctx) {
+        ctx.filter = `brightness(${this.brightness}) contrast(${this.contrast}) `;
     }
 
-
-    changeVideoBackgroundColor(ctx, results) {
+    // Applies a solid background color
+    applyBackgroundColor(ctx, results) {
         const {videoWidth: width, videoHeight: height} = this.videoElement;
         ctx.clearRect(0, 0, width, height);
         ctx.drawImage(results.segmentationMask, 0, 0, width, height);
@@ -97,63 +101,64 @@ class WebrtcBgModifier {
         ctx.fillRect(0, 0, width, height);
         ctx.globalCompositeOperation = 'destination-atop';
         ctx.drawImage(results.image, 0, 0, width, height);
-
-
     }
 
 
-    makeVideoGray(ctx) {
+    // Applies a grayscale effect
+    applyGrayscale(ctx) {
         const {videoWidth: width, videoHeight: height} = this.videoElement;
-        // Get the image data of the frame
         const imageData = ctx.getImageData(0, 0, width, height);
         const data = imageData.data;
 
-        // Apply grayscale filter to each pixel
         for (let i = 0; i < data.length; i += 4) {
-            const r = data[i]; // Red
-            const g = data[i + 1]; // Green
-            const b = data[i + 2]; // Blue
-            const avg = (r + g + b) / 3; // Average of red, green, and blue to create grayscale
-
-            data[i] = avg; // Set red to the average
-            data[i + 1] = avg; // Set green to the average
-            data[i + 2] = avg; // Set blue to the average
+            const avg = (data[i] + data[i + 1] + data[i + 2]) / 3;
+            data[i] = data[i + 1] = data[i + 2] = avg;
         }
 
-        // Put the modified image data back to the canvas
         ctx.putImageData(imageData, 0, 0);
     }
 
-    // Initializes segmentation process for a given video and canvas
+    // Generates a list of available backgrounds
+    backgroundList() {
+        return {
+            color: [
+                {value: '#F9C0AB', alt: '#F9C0AB'},
+                {value: '#F4E0AF', alt: '#F4E0AF'},
+                {value: '#A8CD89', alt: '#A8CD89'},
+                {value: '#355F2E', alt: '#355F2E'},
+            ],
+            image: [
+                {value: 'img/1.jpg', alt: 'Image 1'},
+                {value: 'img/2.jpg', alt: 'Image 2'},
+                {value: 'img/3.jpg', alt: 'Image 3'},
+            ],
+        };
+    }
+
+    // Initializes the segmentation process
     async setupSegmentation(ctx, backgroundImage) {
-        await this.appendScriptToHead('../src/selfie_segmentation.js', {
+        await this.appendScriptToHead('https://cdn.jsdelivr.net/npm/@mediapipe/selfie_segmentation/selfie_segmentation.js', {
             async: true,
-            defer: false,
             callback: async () => {
                 const segmentation = new SelfieSegmentation({
-                    locateFile: (file) => `/node_modules/@mediapipe/selfie_segmentation/${file}`,
+                    // locateFile: (file) => `/node_modules/@mediapipe/selfie_segmentation/${file}`,
+                    locateFile: (file) => `https://cdn.jsdelivr.net/npm/@mediapipe/selfie_segmentation/${file}`,
                 });
 
                 segmentation.setOptions({modelSelection: 1});
 
                 segmentation.onResults((results) => {
-
-
-                    if(this.color){
-                        this.changeVideoBackgroundColor(ctx, results)
-
-                    }else
-                    {
-                        this.changeVideoBackgroundImage(ctx, results, backgroundImage)
-
+                    if (this.color) {
+                        this.applyBackgroundColor(ctx, results);
+                        this.applyBrightnessAndContrast(ctx);
+                    } else {
+                        this.applyBackgroundImage(ctx, results, backgroundImage);
                     }
 
                     if (this.grayScale) {
-                        this.makeVideoGray(ctx)
+                        this.applyGrayscale(ctx);
                     }
-                    this.changeVideoBrightnessAndContrast(ctx)
-
-
+                    // this.applyBrightnessAndContrast(ctx);
                 });
 
                 await segmentation.initialize();
@@ -164,85 +169,36 @@ class WebrtcBgModifier {
                 };
 
                 await processVideo();
-            }
+            },
         });
     }
 
-    // Returns a list of available background images
-    backgroundList() {
-        return [{
-            color: [
-                {value: '#F9C0AB', alt: '#355F2E'},
-                {value: '#F4E0AF', alt: '#F4E0AF'},
-                {value: '#A8CD89', alt: '#A8CD89'},
-                {value: '#355F2E', alt: '#355F2E'},
-            ],
-            image: [
-                {value: 'img/1.jpg', alt: 'image 1'},
-                {value: 'img/2.jpg', alt: 'image 2'},
-                {value: 'img/3.jpg', alt: 'image 3'},
-            ]
-        }
-        ];
-    }
-
-    // Replaces the video track of an existing stream with a new video track
-    async updateVideoTrack(oldStream, newStream) {
-        if (!oldStream || !newStream) {
-            console.error("Old stream or new video track is missing!");
-            return;
-        }
-
-        const oldVideoTrack = oldStream.getVideoTracks()[1];
-        const newVideoTrack = newStream.getVideoTracks()[0];
-
-        try {
-            oldVideoTrack && await oldStream.removeTrack(oldVideoTrack);
-            await oldStream.addTrack(newVideoTrack);
-            console.log("Video track replaced successfully.");
-        } catch (error) {
-            console.error("Error replacing video track:", error);
-        }
-    }
-
-    // Main function for replacing the background in the video stream and setting it to the output video
-    async changeBackground(outputVideo) {
-
-
-        console.log(this.brightness,this.contrast)
-
+    // Main function to modify the video stream
+    async changeBackground() {
         this.videoElement.srcObject = this.stream;
         this.videoElement.play();
 
         await new Promise((resolve) => (this.videoElement.onloadeddata = resolve));
 
         const {videoWidth: width, videoHeight: height} = this.videoElement;
-        this.canvasElement.height = height;
         this.canvasElement.width = width;
+        this.canvasElement.height = height;
         const ctx = this.canvasElement.getContext('2d');
 
-
-        if ((this.url === 'null' || this.url === null) && !this.grayScale && this.brightness === 1 && this.contrast === 1 && !this.color) {
-            outputVideo.srcObject = this.stream;
-            return this.stream; // No background image, return the original stream
+        if (!this.url && !this.grayScale && this.brightness === 1 && this.contrast === 1 && !this.color && !this.blur) {
+            return this.stream; // No modifications, return original stream
         }
-        let backgroundImage = null
-        if (this.url && this.url !== 'null') {
+
+        let backgroundImage = null;
+        if (this.url) {
             backgroundImage = new Image();
             backgroundImage.src = this.url;
             await new Promise((resolve) => (backgroundImage.onload = resolve));
-
         }
 
         await this.setupSegmentation(ctx, backgroundImage);
-
-        const processedStream = this.canvasElement.captureStream(30);
-
-        outputVideo.srcObject = processedStream;
-        // outputVideo.load();
-
-        return processedStream;
+        return this.canvasElement.captureStream(30);
     }
 }
 
-export default WebrtcBgModifier
+export default WebrtcBgModifier;
