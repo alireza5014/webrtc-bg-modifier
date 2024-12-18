8e4339566196aaec4bcffb6d6195466bf467fe99
ar.heidari <ar.heidari@dotin.ir>
2024-12-18 16:05:06 +0330

    fix some bugs

--- example/scripts.js
+++ example/scripts.js
@@ -2,7 +2,7 @@ let bgUrl = null
 let newStream = null
 const outputVideo = document.getElementById("output");
 const isMobile = /Mobi|Android/i.test(navigator.userAgent);
-
+// grantUserMedia
 const stream = await navigator.mediaDevices.getUserMedia({
     video: isMobile ? {
         width: { ideal: 640, max: 640 },   // Ideal width for mobile (480p or 360p)
@@ -16,50 +16,64 @@ outputVideo.srcObject = stream
 outputVideo.play();
 import WebrtcBgModifier from "../dist/index.module.js";
 
-const bgModifier = new WebrtcBgModifier();
+const bgModifier = new WebrtcBgModifier({});
+
 document.getElementById('brightnessRange').addEventListener('change', async function () {
     console.log(bgModifier, "bgModifier")
     const value = this.value;
-    outputVideo.srcObject = await bgModifier.setBackgroundImage(bgUrl).setBrightness(value).setStream(stream).changeBackground();
+    outputVideo.srcObject = await bgModifier.setBackgroundImage(bgUrl).setBrightness(value).setStream(stream).makePreview();
 });
 
 document.getElementById('contrastRange').addEventListener('change', async function () {
     const value = this.value;
-    outputVideo.srcObject = await bgModifier.setBackgroundImage(bgUrl).setContrast(value).setStream(stream).changeBackground();
+    outputVideo.srcObject = await bgModifier.setBackgroundImage(null).setContrast(value).setStream(stream).makePreview();
 });
 
 document.getElementById('blurRange').addEventListener('change', async function () {
     const value = this.value;
-    outputVideo.srcObject = await bgModifier.setBackgroundImage(bgUrl).setBlur(value).setStream(stream).changeBackground();
+    outputVideo.srcObject = await bgModifier.setBackgroundImage(null).setBlur(value).setStream(stream).makePreview();
 });
 document.getElementById('fpsRange').addEventListener('change', async function () {
     const value = this.value;
-    outputVideo.srcObject = await bgModifier.setBackgroundImage(bgUrl).setFps(value).setStream(stream).changeBackground();
+    outputVideo.srcObject = await bgModifier.setBackgroundImage(bgUrl).setFps(value).setStream(stream).makePreview();
 });
 
 document.getElementById('grayScaleSwitch').addEventListener('change', async function () {
     const value = this.checked;
-    outputVideo.srcObject = await bgModifier.setBackgroundImage(bgUrl).setGrayScale(value).setStream(stream).changeBackground();
+    outputVideo.srcObject = await bgModifier.setBackgroundImage(bgUrl).setGrayScale(value).setStream(stream).makePreview();
 });
 // Get the webcam video stream
 
 
 window.changeBg = async function (url) {
     bgUrl = url;
-    newStream = await bgModifier.setBackgroundImage(url).setStream(stream).changeBackground();
+    newStream = await bgModifier.setBackgroundImage(url).setBlur(0).setStream(stream).makePreview();
     outputVideo.srcObject = newStream;
 
 };
+window.addReaction = async function (emoji,x=0,y=0,duration=0) {
+
+    bgModifier.addReaction(emoji,100,300,3000)
+
+};
 window.changeBgColor = async function (color) {
-    newStream = await bgModifier.setBackgroundColor(color).setStream(stream).changeBackground();
+
+    newStream = await bgModifier.setBackgroundColor(color).setStream(stream).makePreview();
     outputVideo.srcObject = newStream;
 
 };
 
 
 const bgListItems = bgModifier.backgroundList()
-let bgListHtml = `<div onclick="changeBg(null)"> <video width="100px"   autoplay playsinline id="default-video" /> </div>`;
+let bgListHtml ='';
+bgListHtml +='<div class="items">';
+bgListHtml += bgListItems.reactions.map(d =>
+    `<div   onclick="addReaction('${d.value}')" style="font-size: 70px;"> ${d.value}</div>`
+).join('')
+bgListHtml +='</div>';
+bgListHtml +='<div class="items">';
 
+bgListHtml+=`<div onclick="changeBg(null)"> <video width="100px"   autoplay playsinline id="default-video" /> </div>`
 bgListHtml += bgListItems.color.map(d =>
     `<div onclick="changeBgColor('${d.value}')">
                         <div style="width: 100px; height: 80px; background: ${d.value}; display: flex; align-items: center; justify-content: center; color: #333; border: 1px solid #ddd; border-radius: 8px;">
@@ -67,15 +81,21 @@ bgListHtml += bgListItems.color.map(d =>
                         </div>
                     </div>`
 ).join('')
+bgListHtml +='</div>';
+bgListHtml +='<div class="items">';
+
 bgListHtml += bgListItems.image.map(d =>
     `<div onclick="changeBg('${d.value}')">
                         <img width="100" src="${d.value}" alt="${d.alt}" style="border: 1px solid #ddd; border-radius: 8px;">
                     </div>`
 )
     .join('');
+bgListHtml +='</div>';
+
 
 document.getElementById("bg-list").innerHTML = bgListHtml
 
 const defaultVideo = document.getElementById("default-video");
 defaultVideo.srcObject = stream
 defaultVideo.play();
+changeBg(null)
--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 class WebrtcBgModifier {
-    constructor() {
+    constructor({chatAgent = null}) {
+
+        console.log(chatAgent,"chatAgent2")
+
         this.scriptLoaded = false;
         this.segmentation = null;
-        this.url = null;
-        this.camera = null;
+        this.bgUrl = null;
         this.backgroundImage = null;
         this.color = null;
+        this.chatAgent = chatAgent;
         this.stream = null;
         this.processing = false;
         this.brightness = 1;
@@ -19,57 +22,112 @@ class WebrtcBgModifier {
         this.videoInfo = {videoWidth: 640, videoHeight: 480};
         this.videoElement = document.createElement("video");
         this.canvasElement = document.createElement("canvas");
+        this.reaction = []
+        this.lastPushTime = 0;
     }
 
-    setFps(value) {
-        this.fps = value;
-        return this;
+
+//private methods
+    drawReaction() {
+        if (this.reaction) {
+            this.reaction.map(data=>{
+
+             this.ctx.globalCompositeOperation = 'source-over';
+            const now = performance.now();
+            const elapsed = now - data.startTime;
+            // Remove the reaction if the duration has passed
+            if (elapsed > data.duration) {
+                data = null;
+                return;
+            }
+            // Calculate the fade-out effect (optional)
+            const progress = elapsed / data.duration;
+            this.ctx.globalAlpha = 1 - progress;
+
+            // Calculate the vertical movement (move from down to up)
+            const moveUpDistance = 300 * progress; // Adjust the distance as needed
+            const shakeAmount = 5 * Math.sin(now / 100); // Shake effect (change frequency as needed)
+
+            // Apply the shake and move-up effects to the position
+            const x = data.x + shakeAmount;
+            const y = data.y - moveUpDistance;
+
+            this.ctx.font = "88px Arial"; // Adjust size as needed
+            this.ctx.fillText(data.emoji, x, y);
+
+            // Reset globalAlpha for other drawings
+            this.ctx.globalAlpha = 1;
+            })
+
+        }
+    }
+
+    // Initializes the segmentation process
+    async setupSegmentation() {
+        const backgroundImage = this.getBackgroundImage()
+        await this.appendScriptToHead('https://cdn.jsdelivr.net/npm/@mediapipe/selfie_segmentation/selfie_segmentation.js', {
+            async: true,
+            callback: async () => {
+
+                if (!this.segmentation) {
+                    this.segmentation = new SelfieSegmentation({
+                        // locateFile: (file) => `/node_modules/@mediapipe/selfie_segmentation/${file}`,
+                        locateFile: (file) => `https://cdn.jsdelivr.net/npm/@mediapipe/selfie_segmentation/${file}`,
+                    });
+
+
+                    this.videoElement.width = this.isMobile ? 480 : 1280;
+                    this.videoElement.height = this.isMobile ? 360 : 720;
+
+                    this.segmentation.setOptions({
+                        modelSelection: 1,  // 1 means use the full-body segmentation model
+                        outputStride: 16,
+                        segmentBody: true,
+                        selfieMode: false,
+                        // modelSelection: this.isMobile ? 0 : 1, // Full model for desktops
+                    });
+
+
+                    this.segmentation.onResults((results) => {
+                        console.log(results)
+                        if (this.color) {
+                            this.applyBackgroundColor(results);
+                            this.applyBrightnessAndContrast();
+                        } else {
+                            this.applyBackgroundImage(results, backgroundImage);
+
+                        }
+                        this.applyGrayscale();
+                        // this.applyBrightnessAndContrast(ctx);
+                    });
+
+
+                    let lastFrameTime = 0;
+
+                    const processVideo = async () => {
+                        if (this.processing) {
+                            const now = performance.now();
+                            if (now - lastFrameTime >= 1000 / this.fps) {
+                                lastFrameTime = now;
+                                await this.segmentation.send({image: this.videoElement});
+                            }
+                        }
+
+                        requestAnimationFrame(processVideo);
+                    };
+
+                    await processVideo();
+
+
+                }
+
+            },
+        });
     }
 
     // Setters for background properties
     setBackgroundImage2(value) {
         this.backgroundImage = value;
-        return this;
-    }
-
-    getBackgroundImage() {
-        return this.backgroundImage
-    }
-
-    setBrightness(value) {
-        this.brightness = +value;
-        return this;
-    }
-
-    setContrast(value) {
-        this.contrast = +value;
-        return this;
-    }
-
-    setBlur(value) {
-        this.blur = +value + "px";
-        return this;
-    }
-
-    setGrayScale(value) {
-        this.grayScale = value;
-        return this;
-    }
-
-    setStream(stream) {
-        this.stream = stream;
-        return this;
-    }
-
-    setBackgroundImage(url) {
-        this.url = url;
-        this.color = null;
-        return this;
-    }
-
-    setBackgroundColor(color) {
-        this.color = color;
-
         return this;
     }
 
@@ -103,17 +161,29 @@ class WebrtcBgModifier {
         if (!results.segmentationMask || !this.getBackgroundImage() && !this.videoElement) {
             return; // No background or segmentation to apply, return early
         }
-        // this.ctx.clearRect(0, 0, width, height);
-        this.ctx.filter = `brightness(${this.brightness}) contrast(${this.contrast}) blur(${this.blur})`;
-        this.ctx.drawImage(results.segmentationMask, 0, 0, width, height);
+
+
+
+
+
+         // this.ctx.globalCompositeOperation = 'destination-over';
+
+         // this.ctx.clearRect(0, 0, width, height);
+         this.ctx.drawImage(results.segmentationMask, 0, 0, width, height);
         this.ctx.globalCompositeOperation = 'source-out';
 
+        this.ctx.filter = `brightness(${this.brightness}) contrast(${this.contrast}) blur(${this.blur})`;
+
+
         this.ctx.drawImage(this.getBackgroundImage() ? this.getBackgroundImage() : this.videoElement, 0, 0, width, height);
+
+        this.drawReaction();
+
         this.ctx.globalCompositeOperation = 'destination-atop';
-
-
         this.ctx.filter = `brightness(${this.brightness}) contrast(${this.contrast})`
-        this.ctx.drawImage(results.image, 0, 0, width, height);
+
+         this.ctx.drawImage(results.image, 0, 0, width, height);
+
 
     }
 
@@ -126,36 +196,132 @@ class WebrtcBgModifier {
     applyBackgroundColor(results) {
 
         const {videoWidth: width, videoHeight: height} = this.videoInfo;
+
         // this.ctx.clearRect(0, 0, width, height);
+
         this.ctx.drawImage(results.segmentationMask, 0, 0, width, height);
+
         this.ctx.globalCompositeOperation = 'source-out';
+
         if (this.color !== this.ctx.fillStyle) {
             this.ctx.fillStyle = this.color;
         }
-        this.ctx.fillRect(0, 0, width, height);
-        this.ctx.globalCompositeOperation = 'destination-atop';
-        this.ctx.drawImage(results.image, 0, 0, width, height);
-    }
 
+        this.ctx.fillRect(0, 0, width, height);
+        this.drawReaction();
+
+        this.ctx.globalCompositeOperation = 'destination-atop';
+
+        this.ctx.drawImage(results.image, 0, 0, width, height);
+
+
+    }
 
     // Applies a grayscale effect
     applyGrayscale() {
-        const ctx = this.ctx;
-        const {videoWidth: width, videoHeight: height} = this.videoInfo;
-        const imageData = this.ctx.getImageData(0, 0, width, height);
-        const data = imageData.data;
+        if (this.grayScale) {
+            const {videoWidth: width, videoHeight: height} = this.videoInfo;
+            const imageData = this.ctx.getImageData(0, 0, width, height);
+            const data = imageData.data;
+            for (let i = 0; i < data.length; i += 4) {
+                const avg = (data[i] + data[i + 1] + data[i + 2]) / 3;
+                data[i] = data[i + 1] = data[i + 2] = avg;
+            }
 
-        for (let i = 0; i < data.length; i += 4) {
-            const avg = (data[i] + data[i + 1] + data[i + 2]) / 3;
-            data[i] = data[i + 1] = data[i + 2] = avg;
+            this.ctx.putImageData(imageData, 0, 0);
         }
+    }
 
-        this.ctx.putImageData(imageData, 0, 0);
+// end private methods
+
+
+
+
+    addReaction(emoji, x = 0, y = 0, duration = 2000) {
+
+        if(!this.segmentation){
+            console.error("Please use reaction after segmentation is available.");
+        }else{
+            const currentTime = performance.now();
+
+            if (currentTime - this.lastPushTime >= 1000) {
+                this.reaction.push({
+                    emoji,
+                    x,
+                    y,
+                    startTime: performance.now(),
+                    duration
+                })
+                this.lastPushTime = currentTime;
+
+            }
+            else{
+                console.log("Waiting for 1 second to pass before adding another reaction.");
+
+            }
+         }
+
+        return this;
+    }
+
+    setFps(value) {
+        this.fps = value;
+        return this;
+    }
+
+    getBackgroundImage() {
+        return this.backgroundImage
+    }
+
+    setBrightness(value) {
+        this.brightness = +value;
+        return this;
+    }
+
+    setContrast(value) {
+        this.contrast = +value;
+        return this;
+    }
+
+    setBlur(value) {
+        this.blur = +value + "px";
+        return this;
+    }
+
+    setGrayScale(value) {
+        this.grayScale = value;
+        return this;
+    }
+
+    setStream(stream) {
+        this.stream = stream;
+        return this;
+    }
+
+    setBackgroundImage(bgUrl) {
+        this.bgUrl = bgUrl;
+        this.color = null;
+        return this;
+    }
+
+    setBackgroundColor(color) {
+        this.color = color;
+        this.bgUrl = null;
+
+        return this;
     }
 
     // Generates a list of available backgrounds
     backgroundList() {
         return {
+            reactions: [
+                {value: '🎉'},
+                {value: '❤️'},
+                {value: '👍️'},
+                {value: '😊'},
+                {value: '😎'},
+                {value: '👎'},
+            ],
             color: [
                 {value: '#f9c0ab', alt: '#F9C0AB'},
                 {value: '#f4e0af', alt: '#F4E0AF'},
@@ -174,77 +340,15 @@ class WebrtcBgModifier {
         };
     }
 
-    // Initializes the segmentation process
-    async setupSegmentation(ctx) {
-        const backgroundImage = this.getBackgroundImage()
-        await this.appendScriptToHead('https://cdn.jsdelivr.net/npm/@mediapipe/selfie_segmentation/selfie_segmentation.js', {
-            async: true,
-            callback: async () => {
-                if (!this.segmentation) {
-                    this.segmentation = new SelfieSegmentation({
-                        // locateFile: (file) => `/node_modules/@mediapipe/selfie_segmentation/${file}`,
-                        locateFile: (file) => `https://cdn.jsdelivr.net/npm/@mediapipe/selfie_segmentation/${file}`,
-                    });
-
-
-                    this.videoElement.width = this.isMobile ? 480 : 1280;
-                    this.videoElement.height = this.isMobile ? 360 : 720;
-
-                    this.segmentation.setOptions({
-                        modelSelection: 1,  // 1 means use the full-body segmentation model
-                        outputStride: 16,
-                        segmentBody: true,
-                        selfieMode: false,
-                        // modelSelection: this.isMobile ? 0 : 1, // Full model for desktops
-                    });
-
-
-                    this.segmentation.onResults((results) => {
-                        // console.log(results)
-                        if (this.color) {
-                            this.applyBackgroundColor(results);
-                            this.applyBrightnessAndContrast();
-                        } else {
-                            this.applyBackgroundImage(results, backgroundImage);
-
-                        }
-
-                        if (this.grayScale) {
-                            this.applyGrayscale();
-                        }
-                        // this.applyBrightnessAndContrast(ctx);
-                    });
-
-
-                    let lastFrameTime = 0;
-
-                    const processVideo = async () => {
-                        if (this.processing) {
-                            const now = performance.now();
-                            if (now - lastFrameTime >= 1000 / this.fps) {
-                                lastFrameTime = now;
-                                await this.segmentation.send({image: this.videoElement});
-                            }
-                        }
-
-                        requestAnimationFrame(processVideo);
-                    };
-
-                    await processVideo();
-
-
-                }
-
-            },
-        });
-    }
-
     // Main function to modify the video stream
     async changeBackground() {
-
+        // this.stream=chatAgent.deviceManager().mediaStreams.getVideoInput();
+        this.chatAgent.deviceManager().grantUserMediaDevicesPermissions({video: {width: this.videoInfo.videoWidth,height: this.videoInfo.videoHeight, framerate: 10}},     (result)=> {
+            console.log(result, "result")
+            this.stream=result.videoStream
         if (!this.videoElement.srcObject) {
             this.videoElement.srcObject = this.stream;
-            await this.videoElement.play();
+              this.videoElement.play();
             // await new Promise((resolve) => (this.videoElement.onloadeddata = resolve));
             this.videoElement.addEventListener('loadedmetadata', () => {
                 this.videoInfo = {videoWidth: this.videoElement.videoWidth, videoHeight: this.videoElement.videoHeight};
@@ -256,24 +360,66 @@ class WebrtcBgModifier {
         }
 
 
-        if (!this.url && !this.grayScale && this.brightness === 1 && this.contrast === 1 && !this.color && !this.blur) {
+        if (!this.bgUrl && !this.grayScale && this.brightness === 1 && this.contrast === 1 && !this.color && !this.blur) {
             this.processing = false;
+            this.chatAgent.replaceVideoStream(this.stream)
+
             return this.stream; // No modifications, return original stream
         }
 
         let backgroundImage = null;
-        if (this.url) {
+        if (this.bgUrl) {
             backgroundImage = new Image();
             backgroundImage.crossOrigin = 'anonymous';
-            backgroundImage.src = this.url;
+            backgroundImage.src = this.bgUrl;
             // await new Promise((resolve) => (backgroundImage.onload = resolve));
-
         }
         this.processing = true
         this.setBackgroundImage2(backgroundImage)
         this.setupSegmentation();
-        return this.canvasElement.captureStream(this.fps);
+        const newStream= this.canvasElement.captureStream(this.fps);
+
+        this.chatAgent.replaceVideoStream(newStream)
+        return  newStream;
+        })
     }
+
+    async makePreview() {
+            if (!this.videoElement.srcObject) {
+                this.videoElement.srcObject = this.stream;
+                this.videoElement.play();
+                // await new Promise((resolve) => (this.videoElement.onloadeddata = resolve));
+                this.videoElement.addEventListener('loadedmetadata', () => {
+                    this.videoInfo = {videoWidth: this.videoElement.videoWidth, videoHeight: this.videoElement.videoHeight};
+                });
+                const {videoWidth: width, videoHeight: height} = this.videoInfo;
+                this.canvasElement.width = width;
+                this.canvasElement.height = height;
+                this.ctx = this.canvasElement.getContext('2d');
+            }
+
+             if (!this.bgUrl && !this.grayScale && this.brightness === 1 && this.contrast === 1 && !this.color && !this.blur  ) {
+                this.processing = false;
+
+                return this.stream; // No modifications, return original stream
+            }
+
+            let backgroundImage = null;
+            if (this.bgUrl) {
+                backgroundImage = new Image();
+                backgroundImage.crossOrigin = 'anonymous';
+                backgroundImage.src = this.bgUrl;
+                // await new Promise((resolve) => (backgroundImage.onload = resolve));
+            }
+            this.processing = true
+            this.setBackgroundImage2(backgroundImage)
+            this.setupSegmentation();
+            const newStream= this.canvasElement.captureStream(this.fps);
+
+            return  newStream;
+
+    }
+
 }
 
 export default WebrtcBgModifier;
